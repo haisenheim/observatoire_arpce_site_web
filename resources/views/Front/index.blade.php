132acab7980ecb82f608a871739777333c82c004
haisenheim <clementessomba@gmail.com>
2023-05-19 22:51:19 +0100

    first

--- resources/views/Front/index.blade.php
+++ resources/views/Front/index.blade.php
@@ -58,31 +58,15 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="active" href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="team.html">Team</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a href="contact.html">Contact</a></li>
+          <li><a class="active" href="/">Accueil</a></li>
+          <li><a href="#">A PROPOS</a></li>
+          <li><a href="#">COOPERATIVES</a></li>
+          <li><a href="#">CACAO</a></li>
+          <li><a href="#">CAFE</a></li>
+
+          <li><a href="#">ACTUALITE</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a class="btn btn-sm btn-warning" href="/login">Connexion</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
