2bef3f8c8f541d0392ec10e38a3295ff7dfe3caf
haisenheim <clementessomba@gmail.com>
2023-05-19 23:07:46 +0100

    first

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 Auth::routes();
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class,'index'])->name('home');
 
 Route::prefix('admin')
     ->namespace('Admin')
