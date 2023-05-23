c0f2714c7006c7e10b7ef406fa7b2c81b2d03251
haisenheim <clementessomba@gmail.com>
2023-05-23 09:47:53 +0100

    first

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,18 +22,18 @@ class UserController extends Controller
 		// get email and password from request
 		$credentials = request(['phone', 'password']);
 		// try to auth and get the token using api authentication
-		if (!$token = auth('api')->attempt($credentials)) {
+		if (!$token = auth()->attempt($credentials)) {
 			// if the credentials are wrong we send an unauthorized error in json format
 			return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        $user = auth('api')->user();
+        $user = auth()->user();
 
 		return response()->json([
 			'token' => $token,
 			'type' => 'bearer', // you can ommit this
-			'expires' => auth('api')->factory()->getTTL() * 60, // time to expiration
-            'user'=>auth('api')->user(),
+			'expires' => auth()->factory()->getTTL() * 60, // time to expiration
+            'user'=>auth()->user(),
 
 		]);
 	}
