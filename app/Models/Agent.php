81fde2e0987defaaa9733cb3302cb5d2e07fdd2e
haisenheim <clementessomba@gmail.com>
2023-05-31 10:19:40 +0100

    first

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+//use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Agent extends Authenticatable implements JWTSubject
+class Agent extends Authenticatable
 {
     //
     use Notifiable;
@@ -25,20 +25,7 @@ class Agent extends Authenticatable implements JWTSubject
         return $this->last_name . " ".$this->first_name;
     }
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+    
 
 
 }
