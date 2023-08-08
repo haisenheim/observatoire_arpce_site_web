e909694ab99a947904e0d1c9f4dd57245c56be5f
haisenheim <clementessomba@gmail.com>
2023-08-08 16:08:28 +0100

    nouvelle interface

--- app/Mail/SendContactMail.php
+++ app/Mail/SendContactMail.php
@@ -34,7 +34,7 @@ class SendContactMail extends Mailable
     public function envelope()
     {
         return new Envelope(
-            from: new Address('info@obs-arpce.cg', 'Observatoire du Numerique Soutenable'),
+            from: new Address('info@alliages-tech.com', 'Observatoire du Numerique Soutenable'),
             subject: "Prise de contact depuis le site internet ",
         );
     }
